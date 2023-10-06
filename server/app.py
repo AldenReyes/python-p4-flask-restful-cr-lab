828805e4e3a7821802eb5c8ef360ec6ae39bad9e
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-10-05 22:10:20 -0700

    Completes lab: adds model and routing to create a restful API

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -16,12 +16,35 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plants_serialized = [plant.to_dict() for plant in plants]
+        response = make_response(plants_serialized, 200)
+        return response
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(), 201)
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant_by_id = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(plant_by_id, 200)
+        return response
+
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
